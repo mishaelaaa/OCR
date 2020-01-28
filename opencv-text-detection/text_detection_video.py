85ef36724332b956fec83965333e321c65e63d2a
Mihaela Petkova <mishippp@gmail.com>
2020-01-28 18:19:39 +0200

    Update text_detection_video.py

--- opencv-text-detection/text_detection_video.py
+++ opencv-text-detection/text_detection_video.py
@@ -69,20 +69,6 @@ def decode_predictions(scores, geometry):
 	return (rects, confidences)
 
 # construct the argument parser and parse the arguments
-
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-east", "--east", type=str, required=True,
-#	help="path to input EAST text detector")
-#ap.add_argument("-v", "--video", type=str,
-#	help="path to optinal input video file")
-#ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
-#	help="minimum probability required to inspect a region")
-#ap.add_argument("-w", "--width", type=int, default=320,
-#	help="resized image width (should be multiple of 32)")
-#ap.add_argument("-e", "--height", type=int, default=320,
-#	help="resized image height (should be multiple of 32)")
-#args = vars(ap.parse_args())
-
 args = {"east":"frozen_east_text_detection.pb", 
         "video" : "cap", 
         "min_confidence":0.5, 
